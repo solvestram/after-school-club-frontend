890e9980d59eaef3604b8e2d6399e93ed00224f3
Siddikjon <66442163+Siddikjon684@users.noreply.github.com>
2021-11-05 14:16:33 +0000

    added more lessons data

--- lessons.js
+++ lessons.js
@@ -1,16 +1,72 @@
 lessons = [
     {
-        title: 'Math',
+        title: 'Art',
         location: 'London',
-        price: 100,
+        price: 80,
+        quantity: 10,
+        image: 'images/art.jpg'
+    },
+    {
+        title: 'Cookery',
+        location: 'London',
+        price: 70,
         quantity: 5,
-        image: 'images/math.jpg'
+        image: 'images/cookery.jpg'
     },
     {
         title: 'English',
         location: 'London',
         price: 90,
-        quantity: 5,
+        quantity: 15,
         image: 'images/english.jpg'
-    }
+    },
+    {
+        title: 'Football',
+        location: 'London',
+        price: 80,
+        quantity: 10,
+        image: 'images/football.jpg'
+    },
+    {
+        title: 'Hockey',
+        location: 'London',
+        price: 120,
+        quantity: 10,
+        image: 'images/hockey.jpg'
+    },
+    {
+        title: 'Math',
+        location: 'London',
+        price: 100,
+        quantity: 15,
+        image: 'images/Math.jpg'
+    },
+    {
+        title: 'Music',
+        location: 'London',
+        price: 80,
+        quantity: 5,
+        image: 'images/music.jpg'
+    },
+    {
+        title: 'Programming',
+        location: 'London',
+        price: 120,
+        quantity: 13,
+        image: 'images/programming.jpg'
+    },
+    {
+        title: 'Science',
+        location: 'London',
+        price: 100,
+        quantity: 14,
+        image: 'images/science.jpg'
+    },
+    {
+        title: 'Tennis',
+        location: 'London',
+        price: 80,
+        quantity: 10,
+        image: 'images/tennis.jpg'
+    },
 ]
