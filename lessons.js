f42e0921ca8dfd9b26c4ba1a87b88045d1b37302
Siddikjon <66442163+Siddikjon684@users.noreply.github.com>
2021-11-05 14:11:17 +0000

    moved lessons data to a separate js file

---
+++ lessons.js
@@ -0,0 +1,16 @@
+lessons = [
+    {
+        title: 'Math',
+        location: 'London',
+        price: 100,
+        quantity: 5,
+        image: 'images/math.jpg'
+    },
+    {
+        title: 'English',
+        location: 'London',
+        price: 90,
+        quantity: 5,
+        image: 'images/english.jpg'
+    }
+]
