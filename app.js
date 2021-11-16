9072c496c3ba2b786b4a98af1b6eb1ef1c1a3761
Siddikjon <66442163+Siddikjon684@users.noreply.github.com>
2021-11-16 21:56:29 +0000

    implemented sorting and other minor changes

--- app.js
+++ app.js
@@ -10,9 +10,54 @@ var app = new Vue({
     },
     methods: {
         addToCart(lesson) {
-            if (lesson.quantity > 0) {
-                lesson.quantity -= 1;
+            if (lesson.space > 0) {
+                lesson.space -= 1;
             }
         }
     },
+    computed: {
+        sortedLessons() {
+            let sorted;
+
+            if (this.sortBy === 'subject') {
+                sorted = this.lessons.sort(
+                    (a, b) => {
+                        if (a.subject > b.subject) return 1;
+                        if (a.subject < b.subject) return -1;
+                        return 0;
+                    }
+                );
+            } else if (this.sortBy === 'location') {
+                sorted = this.lessons.sort(
+                    (a, b) => {
+                        if (a.location > b.location) return 1;
+                        if (a.location < b.location) return -1;
+                        return 0;
+                    }
+                );
+            } else if (this.sortBy === 'price') {
+                sorted = this.lessons.sort(
+                    (a, b) => {
+                        if (a.price > b.price) return 1;
+                        if (a.price < b.price) return -1;
+                        return 0;
+                    }
+                );
+            } else if (this.sortBy === 'space') {
+                sorted = this.lessons.sort(
+                    (a, b) => {
+                        if (a.space > b.space) return 1;
+                        if (a.space < b.space) return -1;
+                        return 0;
+                    }
+                );
+            }
+
+            if (this.sortOrder === 'descending') {
+                sorted = sorted.reverse();
+            }
+
+            return sorted;
+        }
+    }
 })
