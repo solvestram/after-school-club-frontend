a34dac414aa6f05840daa281e7a83325b9db03f6
Siddikjon <66442163+Siddikjon684@users.noreply.github.com>
2021-11-16 21:55:07 +0000

    updated lessons.js

--- lessons.js
+++ lessons.js
@@ -1,69 +1,69 @@
 lessons = [
     {
-        title: 'Art',
+        subject: 'Art',
         location: 'London',
         price: 80,
         space: 5,
         image: 'images/art.jpg'
     },
     {
-        title: 'Cookery',
+        subject: 'Cookery',
         location: 'London',
         price: 70,
         space: 5,
         image: 'images/cookery.jpg'
     },
     {
-        title: 'English',
+        subject: 'English',
         location: 'London',
         price: 90,
         space: 5,
         image: 'images/english.jpg'
     },
     {
-        title: 'Football',
+        subject: 'Football',
         location: 'Birmingham',
         price: 80,
         space: 5,
         image: 'images/football.jpg'
     },
     {
-        title: 'Hockey',
+        subject: 'Hockey',
         location: 'Birmingham',
         price: 120,
         space: 5,
         image: 'images/hockey.jpg'
     },
     {
-        title: 'Math',
+        subject: 'Math',
         location: 'London',
         price: 100,
         space: 5,
         image: 'images/Math.jpg'
     },
     {
-        title: 'Music',
+        subject: 'Music',
         location: 'London',
         price: 80,
         space: 5,
         image: 'images/music.jpg'
     },
     {
-        title: 'Programming',
+        subject: 'Programming',
         location: 'London',
         price: 120,
         space: 5,
         image: 'images/programming.jpg'
     },
     {
-        title: 'Science',
+        subject: 'Science',
         location: 'London',
         price: 100,
         space: 5,
         image: 'images/science.jpg'
     },
     {
-        title: 'Tennis',
+        subject: 'Tennis',
         location: 'Birmingham',
         price: 80,
         space: 5,
